790a7a1c4f1d43a76589108337809fe492f42d4c
unknown <s.vakathanam@Eugene-Dev.livebookings.london>
2015-09-16 12:26:44 +0100

    Added powerpoint slides

---
+++ selenium locally/Hub_withChromealone.bat
@@ -0,0 +1,3 @@
+START java -jar selenium-server-standalone-2.44.0.jar -port 4444 -role hub -newSessionWaitTimeout 25000
+START java -jar selenium-server-standalone-2.44.0.jar -port 4445 -role node -hub http://10.1.8.75:4444/grid/register -browser browserName=chrome,maxInstances=5 -Dwebdriver.chrome.driver=chromedriver.exe
+START java -jar selenium-server-standalone-2.44.0.jar -port 4446 -role node -hub http://10.1.8.75:4444/grid/register -browser browserName=chrome,maxInstances=5 -Dwebdriver.chrome.driver=chromedriver.exe
